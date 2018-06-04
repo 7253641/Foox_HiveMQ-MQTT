95fb0be2ced5dc61d6893d8b5d6cdd01cba430cb
rizkyrwijaya <rizky.r.wijaya@gmail.com>
2018-06-04 09:36:34 +0700

    FINITOOOO

--- Arduino/ProjectManpro/ProjectManpro.ino
+++ Arduino/ProjectManpro/ProjectManpro.ino
@@ -16,40 +16,45 @@ char str_cels[5];
 float weight;
 char str_hum[5];
 String valueString;
-float h,t;
+float h, t;
+int current = 0;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin(8);                /* join i2c bus with address 8 */
-  Wire.onRequest(requestEvent); /* register request event */
+  Wire.onRequest(requestEvent);
   scale.set_scale();
   scale.tare(); //Reset the scale to 0
 }
 
 void loop() {
-  scale.set_scale(calibration_factor);
-  weight = scale.get_units(3);
-  h = dht.readHumidity();
-  t = dht.readTemperature();
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-  if (weight < 0) weight = 0;
-  Serial.println("Publish message: ");
-  dtostrf(h, 3, 1, str_hum);
-  dtostrf(t, 3, 1, str_cels);
-  dtostrf(weight, 4, 3, str_gram);
-  snprintf(msg, 75, "{\"hum\":%s,\"temp\":%s,\"gram\":%s}", str_hum, str_cels, str_gram);
-  Serial.println(str_hum);
-  Serial.println(str_cels);
-  Serial.println(str_gram);
-  Serial.println(msg);
-  delay(100);
+    scale.set_scale(calibration_factor);
+    weight = scale.get_units(3);
+    h = dht.readHumidity();
+    t = dht.readTemperature();
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+    if (weight < 0) weight = 0;
+    Serial.println("Publish message: ");
+    dtostrf(h, 3, 1, str_hum);
+    dtostrf(t, 3, 1, str_cels);
+    dtostrf(weight, 4, 3, str_gram);
+    snprintf(msg, 75, "{\"hum\":%s,\"temp\":%s,\"gram\":%s}", str_hum, str_cels, str_gram);
+    Serial.println(str_hum);
+    Serial.println(str_cels);
+    Serial.println(str_gram);
+    Serial.println(msg);
+    delay(2000);
 }
 
-// function that executes whenever data is requested from master
-void requestEvent() {
-  Serial.print("requested");
+void requestEvent(){
+  Wire.write(msg[current]);
+  current++;
+  if(current == 37){
+    current = 0;
+  }
 }
+
--- ArduinoNodeMCU/NodeMCU/NodeMCU.ino
+++ ArduinoNodeMCU/NodeMCU/NodeMCU.ino
@@ -1,18 +1,111 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <Wire.h>
 
+const char* ssid = "HEHEHEHEHE";
+const char* password = "ahmabalu";
+const char* mqtt_server = "broker.hivemq.com";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+char msg[50];
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      //client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void setup() {
- Serial.begin(9600); /* begin serial for debug */
- Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
+  Serial.begin(9600); /* begin serial for debug */
+  Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void requestData() {
+  int i;
+  for (i = 0; i < 37; i++) {
+    Wire.requestFrom(8, 1);
+    while (Wire.available()) {
+      char c = Wire.read();
+      msg[i] = c;
+    }
+  }
 }
 
 void loop() {
-  Wire.requestFrom(8, 37); /* request & read data of size 13 from slave */
- while(Wire.available()){
-    char c = Wire.read();
-    Serial.println("Request");
-  Serial.print(c);
- }
- Serial.println();
- Serial.println("Request");
- delay(1000);
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  requestData();
+  client.publish("testmanprofoox", msg);
+  Serial.print("Published : ");
+  Serial.println(msg);
+  delay(3000);
 }
